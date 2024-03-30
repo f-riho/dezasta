f03171c8c722de646d373bb33395fd9c35d46ec6
f-riho <riho1992-maha08rei@hotmail.com>
2024-03-30 22:14:47 +0900

    portfolio

--- fujino_portfolio_code/js/script.js
+++ fujino_portfolio_code/js/script.js
@@ -1,24 +1,37 @@
-
-$(function (d) {
+(function (d) {
     var config = {
         kitId: 'nuy0gdd',
         scriptTimeout: 3000,
         async: true
     },
-        h = d.documentElement, t = setTimeout
-    $(function () {
-        h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive ";
-    },
-        config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
-})
-    (document);
+        h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+})(document);
 
-
-
-$(function () {
-    $(".inview").on("inview", function (event, isInView) {
-        if (isInView) {
-            $(this).stop().addClass("is-show");
-        }
+(function ($) {
+    var fadeIn = $('.fade-in');
+    $(window).on('scroll', function () {
+        $(fadeIn).each(function () {
+            var offset = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > offset - windowHeight + 150) {
+                $(this).addClass("scroll-in");
+            }
+        });
     });
 });
+
+$(window).on('scroll', function () {
+
+    $(".JS_ScrollAnimationItem").each(function () {
+        var position = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > position - windowHeight) {
+            $(this).addClass('isActive');
+        }
+    });
+
+});
+
+
