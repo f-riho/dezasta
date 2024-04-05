fc09742f9c155f803a74745d0bb726d46f016c3b
f-riho <riho1992-maha08rei@hotmail.com>
2024-04-05 19:32:25 +0900

    portfolio

--- fujino_portfolio_code/js/script.js
+++ fujino_portfolio_code/js/script.js
@@ -1,3 +1,4 @@
+// font
 (function (d) {
     var config = {
         kitId: 'nuy0gdd',
@@ -6,6 +7,40 @@
     },
         h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
 })(document);
+
+//  marker
+document.addEventListener('DOMContentLoaded', function () {
+
+    var markerText = document.querySelectorAll('.js-marker'); // 監視対象を選択
+    var markerTextArr = Array.prototype.slice.call(markerText); // 監視対象をArrayに変換（IE対策）
+
+    /* IntersectionObserverに渡すコールバック関数 */
+    var cb = function (entries, observer) {
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                /* 監視対象が画面内に入ったときのアクション */
+                entry.target.classList.add('inview'); // 画面内に入った要素にinviewクラスを付与
+                observer.unobserve(entry.target); // 1度発火した後監視を止める
+            }
+
+        });
+    }
+
+    /* IntersectionObserverに渡すコールバック関数 */
+    var options = {
+        rootMargin: "-50px 0px"
+    }
+
+    /* IntersectionObserver初期化 */
+    var io = new IntersectionObserver(cb, options);
+    io.POLL_INTERVAL = 100; // Polyfillの設定
+
+    markerTextArr.forEach(el => {
+        io.observe(el);
+    });
+
+});
 
 // fadein
 $(function ($) {
@@ -62,4 +97,14 @@ var bar = new ProgressBar.Line(splash_text, {//id名を指定
 //アニメーションスタート
 bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
     $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
+
+$(function () {
+
+    // タイマー機能
+    setTimeout(function () {
+        // ページが読み込まれて3秒後にここに記述された処理が発動します。
+        $('#container').fadeOut();
+    }, 3000);
+
 });
